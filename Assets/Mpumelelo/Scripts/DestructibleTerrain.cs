99559fbee4cc6d6acec4562e9e5664869eaf1593
OVENGEE <mpumelelokenneth911@gmail.com>
2026-09-14 02:31:51 +0200

    Added Code for map destruction
    Still incomplete, just a few steps to something that works

--- Assets/Mpumelelo/Scripts/DestructibleTerrain.cs
+++ Assets/Mpumelelo/Scripts/DestructibleTerrain.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DestructibleTerrain : MonoBehaviour
@@ -21,6 +22,38 @@ public class DestructibleTerrain : MonoBehaviour
 
     public void RemoveTerrainAt(Vector2 worldPosition, float radius)
     {
-        float pixelSize = 1 / m_modifiableTexture.Spr
+        float pixelSize = 1 / m_modifiableTexture.Sprite.pixelsPerUnit;
+        int radiusInPixels = Mathf.RoundToInt(radius / pixelSize);
+        List<Vector2Int> affectedPixelAsOffset = GetCircleOffsets(radiusInPixels);
+
+        Vector2Int circleCenterInPixelSpace = m_modifiableTexture.WorldToTexturePosition(worldPosition, m_spriteRenderer.transform);
+
+        ModifyTextureAt(circleCenterInPixelSpace, Color.clear, affectedPixelAsOffset);
+    }
+
+    private void ModifyTextureAt(Vector2Int circleCenterInPixelSpace, Color color, List<Vector2Int> affectedPixelAsOffset)
+    {
+        foreach (Vector2Int offset in affectedPixelAsOffset)
+        {
+            Vector2Int pixelPosition = circleCenterInPixelSpace + offset;
+            m_modifiableTexture.SetPixel(pixelPosition , color);
+        }
+        m_modifiableTexture.ApplyChanges();
+    }
+
+    private List<Vector2Int> GetCircleOffsets(int radiusInPixels)
+    {
+        List<Vector2Int> affectedPixelAsOffset = new List<Vector2Int>();
+        for (int x = -radiusInPixels; x <= radiusInPixels; x++)
+        {
+            for (int y = -radiusInPixels; y <= radiusInPixels; y++)
+            {
+                if (x * x + y * y <= radiusInPixels * radiusInPixels)
+                {
+                    affectedPixelAsOffset.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+        return affectedPixelAsOffset;
     }
 }
--- Assets/Mpumelelo/Scripts/ModifiableTexture.cs
+++ Assets/Mpumelelo/Scripts/ModifiableTexture.cs
@@ -45,5 +45,35 @@ public class ModifiableTexture : MonoBehaviour
             false);
     }
 
+    public Vector2Int WorldToTexturePosition(Vector2 worldPosition, Transform spriteTransform)
+    {
+        Vector2 localPosition = spriteTransform.InverseTransformPoint(worldPosition);
+        //float pixelSize = 1 / m_pixelsPerUnit;
+        int x = Mathf.RoundToInt(localPosition.x * m_pixelsPerUnit + m_sprite.pivot.x);
+        int y = Mathf.RoundToInt(localPosition.y * m_pixelsPerUnit + m_sprite.pivot.y);
+        return new Vector2Int(x, y);
+    }
+
+    public bool IsValidTexturePosition(Vector2Int texturePosition)
+    {
+        return texturePosition.x >= 0 && texturePosition.x < m_texture.width &&
+               texturePosition.y >= 0 && texturePosition.y < m_texture.height;
+    }
+
+    public bool SetPixel(Vector2Int texturePosition, Color color)
+    {
+        if (!IsValidTexturePosition(texturePosition))
+        {
+            return false;
+        }
+        m_texture.SetPixel(texturePosition.x, texturePosition.y, color);
+        return true;
+    }
+
+    public void ApplyChanges()
+    {
+        m_texture.Apply();
+        // RecreateSprite();
+    }
 
 }
