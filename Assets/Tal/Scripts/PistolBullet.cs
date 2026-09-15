82426cce7123966f29385cff776ad50d583a9391
Tal Peretz <talperetz999@gmail.com>
2026-09-15 13:01:15 +0200

    Pistol imp

---
+++ Assets/Tal/Scripts/PistolBullet.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PistolBullet : MonoBehaviour
+{
+    [SerializeField] private float lifetime = 3f;
+
+    private void Start()
+    {
+        // Destroy automatically if it hits nothing
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Add impact effects/damage logic here later
+        Destroy(gameObject);
+    }
+}
