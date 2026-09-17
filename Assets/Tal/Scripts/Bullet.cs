0089439e1ddc66aa4951a34a0ede41c56e33b683
Tal Peretz <talperetz999@gmail.com>
2026-09-17 21:06:29 +0200

    Shotgun implemented

--- Assets/Tal/Scripts/Bullet.cs
+++ Assets/Tal/Scripts/Bullet.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PistolBullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     [SerializeField] private float lifetime = 3f;
 
